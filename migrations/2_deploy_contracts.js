9a85ba5a908290d62a91ac4afb42a856883e8717
Fugue Foundation <fuguefoundation@gmail.com>
2018-10-29 20:24:23 +0800

    minor edits

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,5 @@
-// Config
-const config = require('../truffle.js');
-
 // Contracts
-const Proxy = artifacts.require('./Proxy.sol');
-const EternalStorage = artifacts.require('./EternalStorage.sol');
 const EternalStorageProxy = artifacts.require('./EternalStorageProxy.sol');
-const OwnedUpgradeabilityProxy = artifacts.require('./OwnedUpgradeabilityProxy.sol');
-const UpgradeabilityOwnerStorage = artifacts.require('./UpgradeabilityOwnerStorage.sol');
-const UpgradeabilityProxy = artifacts.require('./UpgradeabilityProxy.sol');
-const UpgradeabilityStorage = artifacts.require('./UpgradeabilityStorage.sol');
-const Ownable = artifacts.require('./test/Ownable.sol');
 const Token_V0 = artifacts.require('./test/Token_V0.sol');
 const Token_V1 = artifacts.require('./test/Token_V1.sol');
 //const Token_V2 = artifacts.require('./test/Token_V2.sol');
